a5790ee5589be3c9f60485b2894b2eb4d5e04106
Keisuke Nemoto <n@on-lab.jp>
2025-05-01 17:35:38 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/minpaku-matching/',
   plugins: [react()],
   server: { port: 3000 }
 })
